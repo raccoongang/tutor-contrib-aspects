92896daee64f0c12acb22c43d08614a7849b38f4
Dmytro Kaliberda <dmytro.kaliberda@raccoongang.com>
2025-10-31 14:04:42 +0200

    feat: Add CELERY_WORKER_EXTRA_ARGS support for Superset deployments

--- tutoraspects/templates/aspects/apps/superset/docker/docker-bootstrap.sh
+++ tutoraspects/templates/aspects/apps/superset/docker/docker-bootstrap.sh
@@ -41,7 +41,7 @@ fi
 
 if [[ "${1}" == "worker" ]]; then
   echo "Starting Celery worker..."
-  celery --app=superset.tasks.celery_app:app worker -Ofair -l INFO
+  celery --app=superset.tasks.celery_app:app worker -Ofair -l INFO ${CELERY_WORKER_EXTRA_ARGS:-}
 elif [[ "${1}" == "beat" ]]; then
   echo "Starting Celery beat..."
   celery --app=superset.tasks.celery_app:app beat --pidfile /tmp/celerybeat.pid -l INFO -s "${SUPERSET_HOME}"/celerybeat-schedule
